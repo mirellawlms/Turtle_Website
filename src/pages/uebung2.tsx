bc4bb384ab11e77439172e7b113c80cb2eb1c185
mirellawlms <willems.mirella.mw@gmail.com>
2023-07-29 12:29:40 +0200

    Urkunde

--- src/pages/uebung2.tsx
+++ src/pages/uebung2.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import { CodeEditor } from "@/components/CodeEditor";
 import { Field, TurtleViewer } from "@/components/TurtleViewer";
 import { Navigation } from "@/components/Navigation";
-import { Typography } from "@mui/joy";
+import { Button, Modal, ModalClose, ModalDialog, Typography } from "@mui/joy";
 import { useEffect, useState } from "react";
 import { ProgressCheck } from "@/components/ProgressCheck";
 
@@ -87,7 +87,7 @@ const maze_mittel: Field[][] = [
   [Field.EMPTY,Field.WALL,Field.EMPTY,Field.EMPTY,Field.EMPTY,Field.WALL,Field.EMPTY,Field.WALL,Field.WALL,Field.EMPTY,],
   [Field.EMPTY,Field.WALL,Field.WALL,Field.WALL,Field.WALL,Field.WALL,Field.EMPTY,Field.EMPTY,Field.EMPTY,Field.EMPTY,],
   [Field.EMPTY,Field.WALL,Field.EMPTY,Field.WALL,Field.EMPTY,Field.WALL,Field.WALL,Field.WALL,Field.EMPTY,Field.WALL,],
-  [Field.EMPTY,Field.EMPTY,Field.EMPTY,Field.EMPTY,Field.EMPTY,Field.EMPTY,Field.WALL,Field.EMPTY,Field.EMPTY,Field.EXIT,]
+  [Field.EMPTY,Field.EMPTY,Field.EMPTY,Field.EMPTY,Field.EMPTY,Field.EMPTY,Field.EMPTY,Field.EMPTY,Field.EMPTY,Field.EXIT,]
 ];
 
 const maze_schwer: Field[][] = [
@@ -100,7 +100,7 @@ const maze_schwer: Field[][] = [
   [Field.EMPTY,Field.WALL,Field.EMPTY,Field.EMPTY,Field.EMPTY,Field.WALL,Field.EMPTY,Field.WALL,Field.WALL,Field.EMPTY,],
   [Field.EMPTY,Field.WALL,Field.WALL,Field.WALL,Field.WALL,Field.WALL,Field.EMPTY,Field.EMPTY,Field.EMPTY,Field.EMPTY,],
   [Field.EMPTY,Field.EMPTY,Field.EMPTY,Field.WALL,Field.EMPTY,Field.WALL,Field.WALL,Field.WALL,Field.EMPTY,Field.WALL,],
-  [Field.EMPTY,Field.EMPTY,Field.EMPTY,Field.EMPTY,Field.EMPTY,Field.EMPTY,Field.EMPTY,Field.WALL,Field.EMPTY,Field.EXIT,]
+  [Field.EMPTY,Field.EMPTY,Field.EMPTY,Field.EMPTY,Field.EMPTY,Field.EMPTY,Field.EMPTY,Field.EMPTY,Field.EMPTY,Field.EXIT,]
 ];
 
 export default function Home() {
@@ -108,13 +108,15 @@ export default function Home() {
   const [code1, setCode1] = useState(code3_1);
   const [code2, setCode2] = useState(code3_2);
   const [code3, setCode3] = useState(code3_3);
+  const [finalOpen, setFinalOpen] = useState(false);
 
   //Progressbalken hier id zusammengerechnet
   const progress_gesamt = () => {
     const items = [
       "id_3_1",
       "id_3_2",
-      "id_3_3",  
+      "id_3_3",
+      "id_urkunde"  
     ];
     let totalProgress = 0;
     for (let index = 0; index < items.length; index++) {
@@ -162,6 +164,7 @@ export default function Home() {
           id_3_1: prog["id_3_1"]??false,
           id_3_2: prog["id_3_2"]??false,
           id_3_3: prog["id_3_3"]??false,
+          id_urkunde: prog["id_urkunde"]??false
         });
         setCode1(data.task.find((item: any)=> item.id === "id_3_1")?.code ?? code3_1);
         setCode2(data.task.find((item: any)=> item.id === "id_3_2")?.code ?? code3_2);
@@ -246,6 +249,37 @@ export default function Home() {
               TaskErstellen("id_3_3", code_eingabe, imZiel??false)}}
           ></CodeEditor>
         </div>
+        <div style={{marginTop:"20px",justifyContent:"center", display:"flex"}}>
+
+        <Button color="success" 
+        disabled={
+          !(progress["id_3_1"] && progress["id_3_2"] && progress["id_3_3"])
+        }
+        onClick={()=> {
+          setProgress({ ...progress, id_urkunde: true });
+          TaskErstellen("id_urkunde", "", true);
+          setFinalOpen(true);
+          }
+        }>
+          Kurs abschließen
+        </Button>
+        <Modal
+        open={finalOpen}
+        onClose={()=>{
+          setFinalOpen(false);
+        }}>
+            <ModalDialog
+              color="success"
+              variant="plain">
+              <ModalClose />
+              <Typography level="h5">Glückwunsch!!!</Typography>
+              <Typography>Super, dank dir habe ich es aus dem Labyrinth zurück zu meiner Familie geschafft!
+                Ich bin sehr stolz auf dich, dass du diesen Kurs absoliviert hast und bedanke mich nochmal herzlich bei dir für deine Hilfe! Ich hoffe ich konnte dir auch weiterhelfen und dir einiges Wissenswertes auf deinen Weg geben! Ich werde meiner Familie alles über unser Abenteuer erzählen. Bis hoffentlich bald mein Freund! -Pebble
+              </Typography>
+              <a href='/inhalt.pdf' download><Button>Urkunde</Button></a>
+            </ModalDialog>
+        </Modal>
+        </div>  
       </Navigation>
     </div>
   );
