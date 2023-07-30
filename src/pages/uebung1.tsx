d9b9244f62c6c1c2668417344fe3ddb684178b58
mirellawlms <willems.mirella.mw@gmail.com>
2023-07-30 19:45:34 +0200

    U1 Schriftanpassung

--- src/pages/uebung1.tsx
+++ src/pages/uebung1.tsx
@@ -120,42 +120,35 @@ export default function Home() {
           </Typography>
           <Typography level="h4">Kann jeder Programmieren?</Typography>
           <div id="algo.1.1"></div>
-          <div>
-            <div className={styles.bildeinruecken}>
-              <div>
-                Diese Frage habe ich mir oft gestellt. Wer eine gute Tasse
-                Kaffee (oder Kakao) machen kann, kann auch programmieren, ist
-                mein Fazit. Viele würden die Ernsthaftigkeit der Aussage in
-                Frage stellen und schmunzeln. Aber abwegig ist diese trozdem
-                nicht. Um eine schöne heiße Tasse Kaffee zu bekommen (French
-                Press), muss eine bestimmte Abfolge von Schritten eingehalten
-                werden.
-              </div>
-              <img src={"/undraw_coffee.svg"} alt="coffeeImage" height={110} />
-            </div>
-            <div className={styles.Container}>
+          <div className={styles.bildeinruecken}>
+            <Typography level="body1">
+              Diese Frage habe ich mir oft gestellt. Wer eine gute Tasse Kaffee
+              (oder Kakao) machen kann, kann auch programmieren, ist mein Fazit.
+              Viele würden die Ernsthaftigkeit der Aussage in Frage stellen und
+              schmunzeln. Aber abwegig ist diese trozdem nicht. Um eine schöne
+              heiße Tasse Kaffee zu bekommen (French Press), muss eine bestimmte
+              Abfolge von Schritten eingehalten werden.
+            </Typography>
+            <img src={"/undraw_coffee.svg"} alt="coffeeImage" height={110} />
+          </div>
+          <div className={styles.Container}>
+            <Typography level="body1">
               1. Wir benötigen ein Wasserkocher, um Wasser richtig zu erhitzen
               <br></br>2. Die gemahlene Kaffeemenge muss für eine Tasse
               abgestimmt sein
               <br></br>3. Die Brühzeit sollte nur wenige Minuten lang sein
               <br></br>4. Gebrühten Kaffee in eine Tasse gießen
-              <br></br>5. Mit Zucker oder Milch bei Bedarf abschmecken
-              <div
-                style={{
-                  display: "flex",
-                  justifyContent: "center",
-                  marginTop: "10px",
-                }}
-              >
-                Perfekt! Das war es.
-              </div>
-            </div>
-            <br></br> Und so ist es auch beim schreiben eines Programms, ich
-            muss klare, logische Abläufe einhalten und mich orientieren. Ein
-            Programm besteht aus einzelnen Anweisungen - sprich Befehlen, die
-            von oben nach unten abgearbeitet werden. Viele Fragen sich, ich weiß
-            gar nicht, womit ich beim Schreiben eines Programmes beginnenn soll?
-            Um zuallerst eine Strukur und Logik in seinen wirren Gedanken zu
+              <br></br>5. Mit Zucker oder Milch bei Bedarf abschmecken Perfekt!
+              <br></br>Das war es!
+            </Typography>
+          </div>
+          <Typography level="body1" sx={{marginTop:"20px"}}>
+            Und so ist es auch beim schreiben eines Programms, ich muss klare,
+            logische Abläufe einhalten und mich orientieren. Ein Programm
+            besteht aus einzelnen Anweisungen - sprich Befehlen, die von oben
+            nach unten abgearbeitet werden. Viele Fragen sich, ich weiß gar
+            nicht, womit ich beim Schreiben eines Programmes beginnenn soll? Um
+            zuallerst eine Strukur und Logik in seinen wirren Gedanken zu
             bekommen, helfen Entwurfstechniken wie Programmablaufplan,
             Struktogramm und die quick and dirty Methode - Pseudocode. Bei der
             Verwendung dieser Entwurfstechnicken sollte man sich immer
@@ -167,33 +160,36 @@ export default function Home() {
             der eine Entwicklungsumgebung zum Eingeben und Testen des Codes
             benötigt wird. Das schauen wir uns auch im nächsten Kapitel auch
             noch, aber alles zu seiner Zeit!
-            <br></br>
-            {/*Programmablaufplan*/}
-            <Typography level="h4" sx={{ marginTop: "10px" }}>
-              Programmablaufplan (PAP)
-            </Typography>
-            <div>
+          </Typography>
+          {/*Programmablaufplan*/}
+          <Typography level="h4" sx={{ marginTop: "10px" }}>
+            Programmablaufplan (PAP)
+          </Typography>
+          <div>
+            <Typography level="body1">
               Ablaufpläne beschreiben welche Anweisungen in welcher Reihenfolge
               ausgeführt werden müssen. Sie verfügen über einen klar definierten
               Start und Endpunkt. Einzelne Abläufe (Prozesse)können dabei wieder
               durch Programmablaufpläne dargestellt werden bis sie vollständig
               durch triviale Anweisungen beschrieben sind.
-              <div className={styles.bildeinruecken2}>
-                <img src={"/PAP.png"} alt="PAPImage" height={300} />
-              </div>
-              <Typography
-                level="body5"
-                sx={{ display: "flex", justifyContent: "center" }}
-              >
-                Bitte schaue für die genaue Erklärung in das GdP-Skript. Dort
-                wird alles nochmal detailliert erklärt.
-              </Typography>
-            </div>
-            {/*Struktogramm*/}
-            <Typography level="h4" sx={{ marginTop: "10px" }}>
-              Struktogramm (Nassi-Shneidermann Diagramm)
             </Typography>
-            <div>
+            <div className={styles.bildeinruecken2}>
+              <img src={"/PAP.png"} alt="PAPImage" height={300} />
+            </div>
+            <Typography
+              level="body5"
+              sx={{ display: "flex", justifyContent: "center" }}
+            >
+              Bitte schaue für die genaue Erklärung in das GdP-Skript. Dort wird
+              alles nochmal detailliert erklärt.
+            </Typography>
+          </div>
+          {/*Struktogramm*/}
+          <Typography level="h4" sx={{ marginTop: "10px" }}>
+            Struktogramm (Nassi-Shneidermann Diagramm)
+          </Typography>
+          <div>
+            <Typography level="body1">
               Struktogramme bieten eine alternative Möglichkeit, Algorithmen
               darzustellen. Ihr Bausteine sind Blöcke, die durch spezielle
               Operationen verschachtelt werden können. Die einfachste Version
@@ -202,66 +198,72 @@ export default function Home() {
               Programmquelltext, da sie eine klare lineare Struktur haben. Sie
               eignen sich besser, einen Programmablauf “von oben nach unten”
               darzustellen.
-              <div className={styles.bildeinruecken2}>
-                <img
-                  src={"/Struktogramm.png"}
-                  alt="StruktogrammImage"
-                  height={300}
-                />
-              </div>
-              <Typography
-                level="body5"
-                sx={{ display: "flex", justifyContent: "center" }}
-              >
-                Bitte schaue für die genaue Erklärung in das GdP-Skript. Dort
-                wird alles nochmal detailliert erklärt.
-              </Typography>
-            </div>
-            {/*Pseudocode*/}
-            <Typography level="h4" sx={{ marginTop: "10px" }}>
-              Pseudocode
             </Typography>
-            <div id="algo.1.2"></div>
-            <div>
+            <div className={styles.bildeinruecken2}>
+              <img
+                src={"/Struktogramm.png"}
+                alt="StruktogrammImage"
+                height={300}
+              />
+            </div>
+            <Typography
+              level="body5"
+              sx={{ display: "flex", justifyContent: "center" }}
+            >
+              Bitte schaue für die genaue Erklärung in das GdP-Skript. Dort wird
+              alles nochmal detailliert erklärt.
+            </Typography>
+          </div>
+          {/*Pseudocode*/}
+          <Typography level="h4" sx={{ marginTop: "10px" }}>
+            Pseudocode
+          </Typography>
+          <div id="algo.1.2"></div>
+          <div>
+            <Typography level="body1">
               Beim Pseudocode soll es sich erstmals um eine Schritt für Schritt
               Anleitung/Beschreibung handeln, die man anschließend in Code
               überführen will. Das Ziel ist es den Algorithmus abstrakt zu
               beschreiben, aber jedoch eine Struktur beizubehalten. Auf einem
               Blatt Papier werden die jeweiligen einzelnen Schritte
               untereinander niedergeschrieben.
-              <Accordion
-                titel="Eine Faustregel"
-                inhalt={
-                  <span>
-                    1. Verstehe das Problem was du Lösen möchtest <br></br>
-                    2. Versuche das Problem in Stücke aufzubrechen, um es besser
-                    zu verstehen<br></br>
-                    3. Schreibe deinen Pseudocode möglichst mit einfachen Worten
-                    <br></br>
-                    4. Teste deinen Pseudocode<br></br>
-                    5. Führe deinen Pseudocode anschließend in ein Programm über
-                  </span>
-                }
-              ></Accordion>
+            </Typography>
+            <Accordion
+              titel="Eine Faustregel"
+              inhalt={
+                <span>
+                  1. Verstehe das Problem was du Lösen möchtest <br></br>
+                  2. Versuche das Problem in Stücke aufzubrechen, um es besser
+                  zu verstehen<br></br>
+                  3. Schreibe deinen Pseudocode möglichst mit einfachen Worten
+                  <br></br>
+                  4. Teste deinen Pseudocode<br></br>
+                  5. Führe deinen Pseudocode anschließend in ein Programm über
+                </span>
+              }
+            ></Accordion>
+            <Typography level="body1">
               Wir schauen uns nun gemeinsam ein Pseudocode Beispiel an.
               Sicherlich kannst du all diese Schritte folgen, da du es selber
               schonmal gemacht hast. Ich spreche natürlich vom "TK-Pizza
               backen". Alle Schritte, die man beim backen einer TK-Pizza macht
               werden nun in ein Pseudocode geschrieben.
-              <div className={styles.bildeinruecken2}>
-                <img src={"/pizza.png"} alt="pizza" height={200} />
-              </div>
-              <Typography
-                level="body5"
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                  marginBottom: "20px",
-                }}
-              >
-                Listing 1:Pizza backen-"Studienvorkurs Informatik" Prof. Dr.
-                Michael Brinkmeier, Dr. Nils Haldenwang, Universtität Osnabrück
-              </Typography>
+            </Typography>
+            <div className={styles.bildeinruecken2}>
+              <img src={"/pizza.png"} alt="pizza" height={200} />
+            </div>
+            <Typography
+              level="body5"
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                marginBottom: "20px",
+              }}
+            >
+              Listing 1:Pizza backen-"Studienvorkurs Informatik" Prof. Dr.
+              Michael Brinkmeier, Dr. Nils Haldenwang, Universtität Osnabrück
+            </Typography>
+            <Typography level="body1">
               In diesem Fall haben wir einen Pseudocode für eine Person mit
               einem Backofen und einer Pizza erstellt. Diesen Pseudocode könnte
               ein Computer nicht ausführen. Es gibt uns (den Pizzabäcker) klare,
@@ -269,69 +271,69 @@ export default function Home() {
               Algorithmus hat verschiedene Sequenzen, die immer nacheinander
               abgearbeitet werden. Es gibt verschiedene Kontrollstrukturen, die
               man beim schreiben eines Algorithmus verwendet.
-              {/*Fallunterscheidung*/}
-              <Typography level="h5" sx={{ marginTop: "10px" }}>
-                Fallunterscheidung
-              </Typography>
-              <div>
-                Die Fallunterscheidung (Bedingte Verzweigung) sagt einem, dass
-                anhand einer Bedingung/Fall entschieden wird, ob eine bestimmte
-                Anweisung durchgeführt wird oder nicht. Am Beispiel des
-                Pizzabäckers, müsste man also die Folie der Pizza entfernen, nur
-                WENN diese in einer Folie eingewickelt ist. <br></br>
-                In der Programmiersprache C++ sind "if" und "else" notwendigen
-                Schlüsselwörter dafür.
-              </div>
-              <div className={styles.bildeinruecken2}>
-                <img src={"/ifelse.png"} alt="ifelse" height={130} />
-              </div>
-              {/*Schleife*/}
-              <Typography level="h5" sx={{ marginTop: "10px" }}>
-                Schleife
-              </Typography>
-              <div>
-                Der Begriff Zyklus (auch Schleife, Wiederholung, Loop) in der
-                Programmierung, bezeichnet die wiederholte Ausführung von Teilen
-                eines Programms. Eine Schleife hat eine Schleifenbedingung, die
-                angibt, wie oft oder bei welcher Bedingung die Schleife
-                ausgeführt werden soll. Wenn man Programme mit Schleifen
-                schreibt oder untersucht, so definiert man sich
-                Schleifeninvarianten. Das sind Bedingungen, die nach jeder
-                Ausführung der Schleife gelten. <br></br>
-                Gehen wir mal davon aus, dass der Pizzabäcker die genaue
-                Backzeit der Pizza nicht weiß, da er die Verpackung weggeworfen
-                hat. <br></br>
-                Hier müsste man SOLANGE die Pizza nochnicht fertig gebacken ist,
-                immer wieder eine Minute warten.
-              </div>
-              <div className={styles.bildeinruecken2}>
-                <img src={"/pizza2.png"} alt="pizza2" height={200} />
-              </div>
-              <Typography
-                level="body5"
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                  marginBottom: "20px",
-                }}
-              >
-                Listing 2:Pizza backen-"Studienvorkurs Informatik" Prof. Dr.
-                Michael Brinkmeier, Dr. Nils Haldenwang, Universtität Osnabrück
-              </Typography>
-              Schleifen sind eine Möglichkeit, Codefragmente wiederholt
-              auszuführen. C++ bietet dafür folgende drei Arten von Anweisungen
-              an:
-              <div className={styles.bildeinruecken2}>
-                <img src={"/zyklus.png"} alt="zyklusImage" height={200} />
-              </div>
-              <Typography
-                level="body5"
-                sx={{ display: "flex", justifyContent: "center" }}
-              >
-                Bitte schaue für die genaue Erklärung in das GdP-Skript. Dort
-                wird alles nochmal detailliert erklärt.
-              </Typography>
+            </Typography>
+            {/*Fallunterscheidung*/}
+            <Typography level="h5" sx={{ marginTop: "10px" }}>
+              Fallunterscheidung
+            </Typography>
+            <Typography level="body1">
+              Die Fallunterscheidung (Bedingte Verzweigung) sagt einem, dass
+              anhand einer Bedingung/Fall entschieden wird, ob eine bestimmte
+              Anweisung durchgeführt wird oder nicht. Am Beispiel des
+              Pizzabäckers, müsste man also die Folie der Pizza entfernen, nur
+              WENN diese in einer Folie eingewickelt ist. <br></br>
+              In der Programmiersprache C++ sind "if" und "else" notwendigen
+              Schlüsselwörter dafür.
+            </Typography>
+            <div className={styles.bildeinruecken2}>
+              <img src={"/ifelse.png"} alt="ifelse" height={130} />
             </div>
+            {/*Schleife*/}
+            <Typography level="h5" sx={{ marginTop: "10px" }}>
+              Schleife
+            </Typography>
+            <Typography level="body1">
+              Der Begriff Zyklus (auch Schleife, Wiederholung, Loop) in der
+              Programmierung, bezeichnet die wiederholte Ausführung von Teilen
+              eines Programms. Eine Schleife hat eine Schleifenbedingung, die
+              angibt, wie oft oder bei welcher Bedingung die Schleife ausgeführt
+              werden soll. Wenn man Programme mit Schleifen schreibt oder
+              untersucht, so definiert man sich Schleifeninvarianten. Das sind
+              Bedingungen, die nach jeder Ausführung der Schleife gelten.{" "}
+              <br></br>
+              Gehen wir mal davon aus, dass der Pizzabäcker die genaue Backzeit
+              der Pizza nicht weiß, da er die Verpackung weggeworfen hat.{" "}
+              <br></br>
+              Hier müsste man SOLANGE die Pizza nochnicht fertig gebacken ist,
+              immer wieder eine Minute warten.
+            </Typography>
+            <div className={styles.bildeinruecken2}>
+              <img src={"/pizza2.png"} alt="pizza2" height={200} />
+            </div>
+            <Typography
+              level="body5"
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                marginBottom: "20px",
+              }}
+            >
+              Listing 2:Pizza backen-"Studienvorkurs Informatik" Prof. Dr.
+              Michael Brinkmeier, Dr. Nils Haldenwang, Universtität Osnabrück
+            </Typography>
+            Schleifen sind eine Möglichkeit, Codefragmente wiederholt
+            auszuführen. C++ bietet dafür folgende drei Arten von Anweisungen
+            an:
+            <div className={styles.bildeinruecken2}>
+              <img src={"/zyklus.png"} alt="zyklusImage" height={200} />
+            </div>
+            <Typography
+              level="body5"
+              sx={{ display: "flex", justifyContent: "center" }}
+            >
+              Bitte schaue für die genaue Erklärung in das GdP-Skript. Dort wird
+              alles nochmal detailliert erklärt.
+            </Typography>
           </div>
           <Typography
             level="h4"
@@ -347,11 +349,10 @@ export default function Home() {
           <div id="algo.1.3"></div>
           <Typography level="body1" sx={{ marginBottom: "20px" }}>
             So das war jetzt aber auch sehr viel Input. Spiel doch gerne zur
-            Belohnung eine kleine Runde Pebble-Simulator. Schlüpfe in meine Rolle als Pebble die Schildkröte
-            und beweg dich aus dem Labyrinth indem du von dem roten
-            Feld auf das grüne Feld gelangst.
+            Belohnung eine kleine Runde Pebble-Simulator. Schlüpfe in meine
+            Rolle als Pebble die Schildkröte und beweg dich aus dem Labyrinth
+            indem du von dem roten Feld auf das grüne Feld gelangst.
           </Typography>
-
           <TurtleViewer_Steuerung
             field={field}
             width={500}
@@ -391,7 +392,7 @@ export default function Home() {
           <Typography sx={{ marginTop: "10px" }} level="body1">
             Teste deinen Pseudocode/Algorithmus gerne mit dem Spiel
             "Pebble-Simulator" durch. Schreibe deinen Pseudocode in dem dafür
-            vorgesehenen Fenster.Um dieses Kapitel abzuschließen musst du deinen
+            vorgesehenen Feld. Um dieses Kapitel abzuschließen musst du deinen
             Pseudocode für eine Peer-Review freigeben. Das bedeutet, dass dein
             Pseudocode einem deiner Komilitonnen zugewiesen wird, der dir darauf
             einen Kommentar gibt, was du beispielsweise noch verbessern kannst,
